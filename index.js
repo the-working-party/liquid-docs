2451d228493a0624452778737aa1470e3358a67c
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-08-11 08:35:36 +1000

    Added CLI app

--- index.js
+++ index.js
@@ -4,7 +4,53 @@ const fs = require("fs");
 const path = require("path");
 const glob = require("glob");
 
-const { parse_files, help } = require("./pkg/twp_types.js");
+const { parse, parse_files, TwpTypes } = require("./pkg/twp_types.js");
+
+function get_files(file_path) {
+	const files = glob.sync(file_path, { cwd: process.cwd() });
+	const file_contents = files.map((file_path) => ({
+		path: file_path,
+		content: fs.readFileSync(path.join(process.cwd(), file_path), "utf8"),
+	}));
+
+	return file_contents;
+}
+
+module.exports = {
+	get_files,
+	parse,
+	parse_files,
+	TwpTypes,
+};
+
+function help() {
+	const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+	console.log(`
+ ▀█▀ █ █ █ █▀█ ▄▄ ▀█▀ █▄█ █▀█ █▀▀ █▀▀
+  █  ▀▄▀▄▀ █▀▀     █   █  █▀▀ ██▄ ▄▄█ v${pkg.version}
+
+A parser for Shopify liquid doc tags
+https://shopify.dev/docs/storefronts/themes/tools/liquid-doc
+
+Usage:
+  twp-types <path>
+
+Description:
+  Tests all Shopify Liquid files for the existence of {% doc %} tags.
+
+Arguments:
+  <path>         Path to a file or directory containing .liquid files.
+                 Can use glob patterns.
+
+Options:
+  -h, --help     Show this help message and exit.
+  -v, --version  Show version information and exit.
+
+Examples:
+  twp-types "{snippets,docs}/*.liquid"
+  twp-types "path/to/snippets/*.liquid"
+`);
+}
 
 const args = process.argv;
 const file_path = args[2] || "./*.liquid";
@@ -20,16 +66,30 @@ if (!file_path || args.includes("-h") || args.includes("--help")) {
 }
 
 if (args.includes("-v") || args.includes("-V") || args.includes("--version")) {
-	const pkg = JSON.parse_files(fs.readFileSync("package.json", "utf8"));
+	const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
 	console.log(`v${pkg.version}`);
 	process.exit(0);
 }
 
-const files = glob.sync(file_path, { cwd: process.cwd() });
-const file_contents = files.map((file_path) => ({
-	path: file_path,
-	content: fs.readFileSync(path.join(process.cwd(), file_path), "utf8"),
-}));
+let found_without_types = 0;
+console.log("Checking files...");
+let data = get_files(file_path);
+parse_files(data).forEach((file) => {
+	if (file.liquid_types) {
+		process.stdout.write("✔️");
+	} else {
+		process.stdout.write("\x1B[31m✖️");
+		found_without_types++;
+	}
+	process.stdout.write(` ${file.path}\x1B[39m\n`);
+});
 
-const liquid_types = parse(file_contents);
-console.log(JSON.stringify(liquid_types, null, 2));
+if (found_without_types > 0) {
+	console.log(
+		`\nFound ${found_without_types} liquid file${found_without_types > 1 ? "s" : ""} without doc tags`,
+	);
+	process.exit(1);
+} else {
+	console.log(`\n✨ All liquid files (${data.length}) have doc tags`);
+	process.exit(0);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use twp_types::TwpTypes;
 #[derive(Debug, Serialize)]
 pub struct LiquidFile {
 	pub path: String,
-	pub liquid_types: Vec<DocBlock>,
+	pub liquid_types: Option<Vec<DocBlock>>,
 }
 
 #[derive(Debug, Default, Serialize, PartialEq)]
@@ -50,18 +50,23 @@ pub fn parse_files(input: JsValue) -> Result<JsValue, JsValue> {
 
 	for file in files {
 		if let Some(blocks) = TwpTypes::extract_doc_blocks(&file.content) {
-			let mut liquid_file = LiquidFile {
-				path: file.path,
-				liquid_types: Vec::with_capacity(blocks.len()),
-			};
+			let mut liquid_types = Vec::with_capacity(blocks.len());
 
 			for block in blocks {
 				if let Ok(block_type) = TwpTypes::parse_doc_content(block) {
-					liquid_file.liquid_types.push(block_type);
+					liquid_types.push(block_type);
 				}
 			}
 
-			all_files.push(liquid_file);
+			all_files.push(LiquidFile {
+				path: file.path,
+				liquid_types: Some(liquid_types),
+			});
+		} else {
+			all_files.push(LiquidFile {
+				path: file.path,
+				liquid_types: None,
+			});
 		}
 	}
 
@@ -79,21 +84,4 @@ pub fn parse(input: String) -> Result<JsValue, JsValue> {
 		}
 	}
 	serde_wasm_bindgen::to_value(&liquid_types).map_err(|e| JsValue::from_str(&e.to_string()))
-}
-
-#[wasm_bindgen]
-pub fn help() -> String {
-	// TODO: write proper help
-	format!(
-		r#"
- ▀█▀ █ █ █ █▀█ ▄▄ ▀█▀ █▄█ █▀█ █▀▀ █▀▀
-  █  ▀▄▀▄▀ █▀▀     █   █  █▀▀ ██▄ ▄▄█ v{}
-
-A parser for Shopify liquid doc tags
-https://shopify.dev/docs/storefronts/themes/tools/liquid-doc
-
-Usage: twp-types <path>
-"#,
-		env!("CARGO_PKG_VERSION")
-	)
 }
